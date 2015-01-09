96c2a059e84b39b63e040594682619766c60bcd2
周峰 <15802743862@163.com>
2015-01-10 00:53:25 +0800

    Update stl_numeric.h

--- stl_numeric.h
+++ stl_numeric.h
@@ -182,7 +182,7 @@ _Tp __power(_Tp __x, _Integer __n, _MonoidOperation __opr)
   if (__n == 0)
     return identity_element(__opr);
   else {
-    while ((__n & 1) == 0) {
+    while ((__n & 1) == 0) {//寻找n中的最右边的1
       __n >>= 1;
       __x = __opr(__x, __x);
     }
@@ -197,6 +197,29 @@ _Tp __power(_Tp __x, _Integer __n, _MonoidOperation __opr)
     }
     return __result;
   }
+}
+//另一版本
+template<class _Tp,class _Integer,class _MonoidOperation>
+_Tp __power(_Tp __x,_Integer __n,_MonoidOperation __opr)
+{
+  if(__n==0)return identity_element(__opr);
+  _Tp __result;
+  if(__n&1==0)__result=1;
+  else __result=__x;
+  __n>>=1;
+  while(__n!=0){
+    __x=__opr(__x,__x);
+    if((__n&1)!=0)__result=__opr(__result,__X);
+    __n>>=1;
+  }
+  return __result;
+}
+//其中__n可以为任意的整数
+template<class _Tp,class _Integer,class _MonoidOperation>
+_Tp __guard_power(_Tp __x,_Integer __n,_MonoidOperation __opr)
+{
+  if(__n<0)return 1/__power(__x,-__n,__opr);
+  return __power(__x,__n,__opr);
 }
 
 template <class _Tp, class _Integer>

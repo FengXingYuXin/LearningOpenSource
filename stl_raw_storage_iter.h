916f4bb8914107d672e954239b189a7c89eb71ec
周峰 <15802743862@163.com>
2015-01-16 19:08:57 +0800

    Update stl_raw_storage_iter.h

--- stl_raw_storage_iter.h
+++ stl_raw_storage_iter.h
@@ -32,7 +32,24 @@
 #define __SGI_STL_INTERNAL_RAW_STORAGE_ITERATOR_H
 
 __STL_BEGIN_NAMESPACE
+//In C++, the operator new allocates memory for an object and then creates 
+//an object at that location by calling a constructor. Occasionally, however, 
+//it is useful to separate those two operations. [1] If i is an iterator that 
+//points to a region of uninitialized memory, then you can use construct to 
+//create an object in the location pointed to by i. Raw_storage_iterator is 
+//an adaptor that makes this procedure more convenient. If r is a raw_storage_iterator, 
+//then it has some underlying iterator i. The expression *r = x is 
+//equivalent to construct(&*i, x).
 
+//Regular iterators operate on a certain type of objects, which have already 
+//been constructed. A raw_storage_iterator wraps one of these regular iterators 
+//into a special output iterator which constructs objects at the location 
+//being pointed before being written.
+
+//一个迭代器配接器
+//raw_storage_iterator:用来在未初始化的内存上进行相应的操作
+//example:copy(begin(),end(),raw_storage_iterator<int*,int>(_start)):
+//在_start指向的原始内存上以(begin(),end())为初始值进行复制
 template <class _ForwardIterator, class _Tp>
 class raw_storage_iterator {
 protected:

ab4bddfe837b53507b0668ec7943f43930a9ae8e
周峰 <15802743862@163.com>
2015-01-22 11:29:37 +0800

    Update defalloc.h

--- defalloc.h
+++ defalloc.h
@@ -61,10 +61,12 @@ public:
     typedef const T& const_reference;
     typedef size_t size_type;
     typedef ptrdiff_t difference_type;
+    
     pointer allocate(size_type n) { 
 	return ::allocate((difference_type)n, (pointer)0);
     }
     void deallocate(pointer p) { ::deallocate(p); }
+    
     pointer address(reference x) { return (pointer)&x; }
     const_pointer const_address(const_reference x) { 
 	return (const_pointer)&x; 

824e20a0b606eb944858f5f018ff90aaf029a10c
周峰 <15802743862@163.com>
2015-01-30 17:42:24 +0800

    Update stl_vector.h

--- stl_vector.h
+++ stl_vector.h
@@ -7,35 +7,7 @@
 //vector设计的关键之处：
 //A 空间配置策略：__allocate_length=empty()?1:2*size()；结合空间利用率以及复制的效率，倍增的方法效果最好
 //B 空间管理策略：_M_first,_M_finish,_M_end_of_storage：尺寸和容量的概念
-/*
- *
- * Copyright (c) 1994
- * Hewlett-Packard Company
- *
- * Permission to use, copy, modify, distribute and sell this software
- * and its documentation for any purpose is hereby granted without fee,
- * provided that the above copyright notice appear in all copies and
- * that both that copyright notice and this permission notice appear
- * in supporting documentation.  Hewlett-Packard Company makes no
- * representations about the suitability of this software for any
- * purpose.  It is provided "as is" without express or implied warranty.
- *
- *
- * Copyright (c) 1996
- * Silicon Graphics Computer Systems, Inc.
- *
- * Permission to use, copy, modify, distribute and sell this software
- * and its documentation for any purpose is hereby granted without fee,
- * provided that the above copyright notice appear in all copies and
- * that both that copyright notice and this permission notice appear
- * in supporting documentation.  Silicon Graphics makes no
- * representations about the suitability of this software for any
- * purpose.  It is provided "as is" without express or implied warranty.
- */
 
-/* NOTE: This is an internal header file, included by other STL headers.
- *   You should not attempt to use it directly.
- */
 
 #ifndef __SGI_STL_INTERNAL_VECTOR_H
 #define __SGI_STL_INTERNAL_VECTOR_H
